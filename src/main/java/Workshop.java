04a4eb409bf52c0436dc3a528397def38ac2507b
Esteban <estebanq1109@gmail.com>
2026-09-03 22:30:29 -0500

    Implementar fibonacci

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -63,8 +63,16 @@ public class Workshop {
         // TODO: Implementar el método para generar la serie de Fibonacci hasta el número n.
         // Ejemplo: Si n = 5, el resultado debería ser [0, 1, 1, 2, 3].
         // Lanzar IllegalArgumentException si n es negativo.
-        return new int[0];
+        if (n <= 0) return 0;
+    if (n == 1) return 1;
+    int a = 0, b = 1, c = 0;
+    for (int i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
     }
+    return b;
+}
 
     // Método que suma todos los elementos de un arreglo
     public int sumaElementos(int[] arreglo) {
