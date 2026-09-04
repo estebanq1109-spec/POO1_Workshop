08a13a9a1ed9bbb9870aa50a0138f174be1dff2f
Esteban <estebanq1109@gmail.com>
2026-09-03 21:39:46 -0500

    Implementar factorial

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -37,7 +37,14 @@ public class Workshop {
         // TODO: Implementar el método para calcular el factorial de un número entero.
         // Ejemplo: Si n = 5, el resultado debería ser 120.
         // Lanzar IllegalArgumentException si n es negativo.
-        return 0;
+        if (n < 0) {
+        throw new IllegalArgumentException("El número no puede ser negativo");
+    }
+    int resultado = 1;
+    for (int i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
     }
 
     // Método que verifica si un número es primo
